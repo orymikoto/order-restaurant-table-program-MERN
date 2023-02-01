90918065d4a2e287f58cba39a96779ad9a77f17b
orymikoto <rosbergroll99@gmail.com>
2023-02-01 23:01:25 +0700

    testing github user

--- backend/controller/users-controller.js
+++ backend/controller/users-controller.js
@@ -1,8 +1,18 @@
 // Import NPM package
 
 // Import local package
+// import user_model from '../data/model/users.js'
 
 // Sign-in / Login function
+export const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const existing_user = await user_model.findOne({ email })
+  } catch (error) {
+    
+  }
+}
+
 
 // Sign-up / Register function
 
