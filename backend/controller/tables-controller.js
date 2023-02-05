291e4b283709584b8dacd7011f33bea81f22f54b
orymikoto <rosbergroll99@gmail.com>
2023-02-05 18:11:48 +0700

    update table & delete table function, add cond

--- backend/controller/tables-controller.js
+++ backend/controller/tables-controller.js
@@ -17,9 +17,12 @@ export const get_all_tables = async (req, res) => {
 export const create_table = async (req, res) => {
   try {
     const {chairs_amount, room_location, table_model} = req.body
-    if (!chairs_amount || !room_location || !table_model){
+
+    // Check empty field
+    if (!chairs_amount || !room_location || !table_model){ 
       return res.status(400).json({message: 'Request not complete'})
     }
+
     const new_table = await table_schema.create({
       chairs_amount: chairs_amount,
       room_location: room_location,
@@ -31,8 +34,56 @@ export const create_table = async (req, res) => {
   }
 }
 
-// Update user password punction
+// Update table function
+export const update_table = async (req, res) => {
+  try {
+    const { table_id, chairs_amount, room_location, table_model} = req.body
 
-// Update user profile function
+    // Check empty field
+    if (!table_id || !chairs_amount || !room_location || !table_model){ 
+      return res.status(400).json({message: 'Request not complete'})
+    }
 
-// Deactivate user profile function
+    //check is table exist
+    const check_table = await table_schema.findById({table_id})
+    if (!check_table){
+      return res.status(404).json({message: `Table with id ${table_id} not exist.`})
+    }
+
+    const updated_table = await table_schema.findByIdAndUpdate({table_id}, {
+      chairs_amount: chairs_amount,
+      room_location: room_location,
+      table_model: table_model
+    })
+
+    return res.status(200).json({message: 'Table updated', data: updated_table})
+  } catch (error) {
+    return res.status(500).json({message: 'something went wrong.', error: error})
+  }
+}
+
+// Soft Delete Table 
+export const soft_delete_table = async (req, res) => {
+  try {
+    const table_id = req.body.table_id
+
+    // check if table id field is not empty
+    if (!table_id){ 
+      return res.status(400).json({message: 'You need to fill table id information first!'})
+    }
+
+    //check is table exist
+    const check_table = await table_schema.findById({table_id})
+    if (!check_table){
+      return res.status(404).json({message: `Table with id ${table_id} not exist.`})
+    }
+
+    const soft_deleted_table = await table_schema.findByIdAndUpdate({table_id}, {
+      still_exist: false
+    })
+
+    return res.status(200).json({message: 'Table has been soft-deleted', data: soft_deleted_table})
+  } catch (error) {
+    return res.status(500).json({message: 'something went wrong.', error: error})
+  }
+}
--- backend/data/model/tables.js
+++ backend/data/model/tables.js
@@ -6,7 +6,8 @@ const table_schema = mongoose.Schema({
   chairs_amount: {type: Number, require: true},
   room_location: {type: String, require: true},
   table_model: {type: String, require: true},
-  real_time_occupation: {type: Boolean, default: false}
+  real_time_occupation: {type: Boolean, default: false},
+  still_exist: {type: Boolean, default: true}
 }, {collection: 'tables'})
 
 // module.exports = new mongoose.model('user_schema', user_schema) // export user schema
