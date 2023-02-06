1a7e64c468ad53cb8c5dfba5782bd64fdf97ee43
orymikoto <rosbergroll99@gmail.com>
2023-02-07 03:19:44 +0700

    check table id order and create table with user

--- backend/controller/orders-controller.js
+++ backend/controller/orders-controller.js
@@ -2,6 +2,7 @@
 import dotenv from 'dotenv'
 import moment from 'moment'
 import 'moment-timezone'
+import jwt from 'jsonwebtoken'
 
 // Import local package
 import order_schema from '../data/model/orders.js'
@@ -19,7 +20,7 @@ export const get_all_future_table_orders = async (req, res) => {
 
     //Guide on how to parse the date
     // console.log(moment(table_orders[0].rent_time.start).tz('Asia/Jakarta').format("YYYY-MM-DD HH:mm"))
-    
+
     return res.status(200).json({message: 'All future table orders is retrieved', data: table_orders})
   } catch (error) {
     console.log( error );
@@ -59,12 +60,71 @@ export const get_table_by_date = async (req, res) => {
 
 // Check table orders using table_id on certain time 
 // Users
+export const check_table_id_order_with_time = async (req, res) => {
+  try {
+    const {table_id, start_time, end_time} = req.body
+    const check_table_order = await order_schema.find({
+      table_id: table_id,
+      "rent_time.start": {$gte: start_time},
+      "rent_time.end": {$lte: end_time}
+    })
+    if(check_table_order.length() < 1){
+      return res.status(200).json({message: 'Schedule is available', available: true})
+    }
+    return res.status(200).json({message: 'Schedule is not available', available: false, data: check_table_order})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message: 'Something went wrong', error: error})
+  }
+}
 
 // Check table order that is going to happens using table_id
 // Users
+export const check_table_id_order = async (req, res) => {
+  try {
+    const {table_id} = req.body
+    const check_table_order = await order_schema.find({
+      table_id: table_id,
+      "rent_time.start": {$gt: moment()},
+    })
+    if(check_table_order.length() < 1){
+      return res.status(200).json({message: 'This table does not have any schedule'})
+    }
+    return res.status(200).json({message: 'This table have some order schedule', data: check_table_order})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message: 'Something went wrong', error: error})
+  }
+}
 
 // Create table order with login user function
 // Users
+export const create_users_table_order = async (req, res) => {
+  try {
+    const {table_id, start_time, end_time, customer_phone_number, customer_name} = req.body 
+    const new_table_order = await order_schema.create({
+      table_id: table_id,
+      rent_time: {
+        start: moment(start_time).tz('Asia/Jakarta').toDate(),
+        end: moment(end_time).tz('Asia/Jakarta').toDate()
+      },
+      customer_name: customer_name,
+      customer_phone_number: customer_phone_number
+    })
+
+    const user_info = jwt.decode(req.headers['authorization'].split(" ")[1])
+    const save_to_user = await user_schema.findByIdAndUpdate(user_info.id, {
+      $push: {
+        table_ordered: new_table_order._id
+      }
+    })
+
+    return res.status(200).json({message: "Table order successfully created and pushed data id to user", data: new_table_order, user_email: save_to_user.email})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message: 'Something went wrong', error: error})
+  }
+}
 
 // Create table order without user function
 // Admins
@@ -92,5 +152,3 @@ export const create_admin_table_order = async (req, res) => {
 
 // Delete table order function
 // Admin
-
-// Deactivate user profile function
