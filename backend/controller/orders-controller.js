d82081732d8a0d1a0b6e5090335764f6309a519f
orymikoto <rosbergroll99@gmail.com>
2023-02-07 23:48:17 +0700

    finish function and fix order-table controller

--- backend/controller/orders-controller.js
+++ backend/controller/orders-controller.js
@@ -16,7 +16,8 @@ dotenv.config()
 // Admin
 export const get_all_future_table_orders = async (req, res) => {
   try {
-    const table_orders = await order_schema.find({ "rent_time.start" : {$gte: moment().startOf('day').toDate()}})
+    // console.log(moment().tz('Asia/Jakarta').toDate());
+    const table_orders = await order_schema.find({ "rent_time.start" : {$gte: moment().tz('Asia/Jakarta').toDate()}})
 
     //Guide on how to parse the date
     // console.log(moment(table_orders[0].rent_time.start).tz('Asia/Jakarta').format("YYYY-MM-DD HH:mm"))
@@ -45,10 +46,11 @@ export const get_all_table_orders = async (req, res) => {
 export const get_table_by_date = async (req, res) => {
   try {
     const {req_date} = req.body
-    const exact_date = moment(str, req_date).startOf(req_date).toDate()
+    const exact_date = moment(req_date).tz('Asia/Jakarta').startOf(req_date).toDate()
+    // console.log(moment(req_date).startOf(req_date).toDate());
     const table_orders = await order_schema.find({"rent_time.start": {
       $gte: exact_date,
-      $lt: exact_date.add(1, 'day')
+      $lt: moment(req_date).tz('Asia/Jakarta').startOf(req_date).add(1, 'day').toDate()
     }})
 
     return res.status(200).json({message: `Table orders at ${req_date} is retrieved`, data: table_orders})
@@ -63,12 +65,14 @@ export const get_table_by_date = async (req, res) => {
 export const check_table_id_order_with_time = async (req, res) => {
   try {
     const {table_id, start_time, end_time} = req.body
-    const check_table_order = await order_schema.find({
-      table_id: table_id,
-      "rent_time.start": {$gte: start_time},
-      "rent_time.end": {$lte: end_time}
-    })
-    if(check_table_order.length() < 1){
+    // console.log(moment(start_time).tz('Asia/Jakarta').toDate());
+    const check_table_order = await order_schema.find(
+      { table_id: table_id, $or: [{
+        $and: [{"rent_time.start": {$gte: moment(start_time).toDate()}}, {"rent_time.start": {$lte: moment(end_time).toDate()}}],
+        $and: [{"rent_time.end": {$gte: moment(start_time).toDate()}}, {"rent_time.start": {$lte: moment(end_time).toDate()}}]
+      }]}
+    )
+    if(check_table_order.length < 1){
       return res.status(200).json({message: 'Schedule is available', available: true})
     }
     return res.status(200).json({message: 'Schedule is not available', available: false, data: check_table_order})
@@ -85,9 +89,9 @@ export const check_table_id_order = async (req, res) => {
     const {table_id} = req.body
     const check_table_order = await order_schema.find({
       table_id: table_id,
-      "rent_time.start": {$gt: moment()},
+      "rent_time.start": {$gt: moment().tz('Asia/Jakarta').toDate()},
     })
-    if(check_table_order.length() < 1){
+    if(check_table_order.length < 1){
       return res.status(200).json({message: 'This table does not have any schedule'})
     }
     return res.status(200).json({message: 'This table have some order schedule', data: check_table_order})
@@ -149,6 +153,46 @@ export const create_admin_table_order = async (req, res) => {
 
 // Delete table order function
 // Users
+export const delete_table_order_users = async (req, res) => {
+  try {
+    const { table_order_id } = req.body
+    const user_info = jwt.decode(req.headers['authorization'].split(" ")[1])
+    
+    const deleted_table_order = await order_schema.findByIdAndDelete(table_order_id)
+
+    if(!deleted_table_order){
+      return res.status(404).json({message: `Table order with id ${table_order_id} not found`})
+    }
+
+    await user_schema.findByIdAndUpdate(user_info.id, {
+      $pull: {
+        table_ordered: {
+          _id: table_order_id
+        }
+      }
+    })
+
+    return res.status(200).json({message: 'Table order successfully deleted', data: deleted_table_order})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message: 'Something went wrong', error: error})
+  }
+}
 
 // Delete table order function
 // Admin
+export const delete_table_order_admin = async (req, res) => {
+  try {
+    const { table_order_id } = req.body
+    const deleted_table_order = await order_schema.findByIdAndDelete(table_order_id)
+    
+    if(!deleted_table_order){
+      return res.status(404).json({message: `Table order with id ${table_order_id} not found`})
+    }
+
+    return res.status(200).json({message: 'Table order successfully deleted', data: deleted_table_order})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message: 'Something went wrong', error: error})
+  }
+}
--- backend/routes/orders-routes.js
+++ backend/routes/orders-routes.js
@@ -3,15 +3,22 @@ import express from 'express'
 
 // Import local package
 import * as controller from '../controller/orders-controller.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 // Router Admin
 router.get('/', controller.get_all_future_table_orders)
-router.post('/admin', controller.create_admin_table_order)
-router.patch('/', /*to do */)
-router.delete('/', /*to do */)
+router.get('/all-table-orders', controller.get_all_table_orders)
+router.post('/all-by-date', controller.get_table_by_date)
+router.post('/create-admin', controller.create_admin_table_order)
+router.delete('/delete-admin', controller.delete_table_order_admin)
 
 // Router Users
+router.post('/check-table-future', auth, controller.check_table_id_order)
+router.post('/check-table-time', auth, controller.check_table_id_order_with_time)
+router.post('/create-user', auth, controller.create_users_table_order)
+router.delete('/delete-user', auth, controller.delete_table_order_users)
+
 
 export default router
