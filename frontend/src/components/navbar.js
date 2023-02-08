b58a8e0ab25361893889555f03872a0e287f07a4
orymikoto <rosbergroll99@gmail.com>
2023-02-08 18:40:50 +0700

    set up react js

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className=' flex '>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default navbar
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,15 @@
+// NPM Package
+import React from 'react'
+
+// Local Components Package
+import navbar from '../components/navbar'
+
+function home() {
+  return (
+    <>
+      <navbar />
+    </>
+  )
+}
+
+export default home
