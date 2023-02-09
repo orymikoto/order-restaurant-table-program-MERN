9c96bd066212099acbb9129446ca31c001eff424
orymikoto <rosbergroll99@gmail.com>
2023-02-09 11:45:05 +0700

    update design home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
   return (
     <div className='bg-yellow-400'>
       <Navbar />
-      <div className='flex w-full py-7 items-center pb-7 bg-yellow-400'>
+      <div className='flex py-7 items-center pb-7 bg-yellow-400'>
         <div className="flex flex-1 flex-col ml-[7rem]">
           <p className='text-xl text-white font-semibold'>Bring your taste to the next level!</p>
           <h3 className='text-4xl font-unbounded text-teal-400'>MIKOTO RESTAURANT</h3>
@@ -21,15 +21,18 @@ function Home() {
           <button className='my-2 hover:bg-fuchsia-700/25 ease-in-out duration-200 hover:border-pink-600 hover:text-pink-600 rounded-lg border-[0.15rem] w-[14rem] text-pink-700 font-medium border-pink-700 text-2xl h-[3rem]'>RENT TABLE</button>
         </div>
         <div className="relative h-[700px] flex-1 items-center justify-center">
-          <img className='absolute m-auto w-[700px]' src="/assets/circle.png" alt="circle" />
-          <img className='absolute m-auto translate-x-[5.5rem] translate-y-[5rem] w-[500px]' src='/assets/steak.png'  alt='steakpicture'/>
+          <div className=' flex relative h-full w-full bg-contain justify-center items-center bg-center bg-no-repeat' style={{ backgroundImage: 'url("/assets/circle.png")'}} >
+            {/* <img className='absolute  w-[100%]' src="/assets/circle.png" alt="circle" /> */}
+            <div className=' relative h-full bg-contain  bg-center bg-no-repeat w-[70%] mb-5 mr-3' style={{backgroundImage: 'url("/assets/steak.png")'}}></div>
+            {/* <img className='absolute  w-[70%]' src='/assets/steak.png'  alt='steakpicture'/> */}
+          </div>
         </div>
       </div>
       <div className='flex bg-yellow-400 flex-col w-full'>
         <div className='m-auto border-t-4 w-[21vw] text-center mb-10'>
           <h3 className='text-3xl mt-2 font-bold text-neutral-100'>MENU</h3>
         </div>
-        <div className='flex gap-10 justify-center w-full mx-7 mb-7'>
+        <div className='flex gap-10 justify-center w-[90%] mx-auto mb-7'>
           <div className='drop-shadow-lg hover:shadow-2xl ease-in-out duration-500 cursor-default hover:-translate-x-1 hover:-translate-y-1 w-[21rem] rounded-2xl bg-white'>
             <h4 className='text-neutral-800 text-2xl font-bold my-2 text-center'>Menu Name</h4>
             <img className='h-[13rem] m-auto my-7' src='/assets/coffee.png' alt="menu1"/>
@@ -56,9 +59,9 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className='flex my-7 flex-col w-[90%] m-auto'>
+      <div className='flex my-7 flex-col w-[85%] m-auto'>
         <div className='flex gap-x-7'>
-          <div className='relative rounded-3xl overflow-hidden min-w-[35%]'>
+          <div className='relative rounded-3xl overflow-hidden min-w-[40%]'>
             <LeftSliderRightDesc  />
           </div>
           <div className='flex flex-col'>
